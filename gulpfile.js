715fb2cbd417027e604092f94326acf488507429
Bert Pardaens <bert.pardaens@persgroep.be>
2014-12-18 16:35:37 +0100

    Start new project

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp'),
+	gutil = require('gulp-util'),
+	sass = require('gulp-sass'),
+	rename = require('gulp-rename'),
+	watch = require('gulp-watch'),
+	pkg = require('./package.json');
+
+gulp.task('sass', function () {
+    gulp.src('./sass/*.scss')
+        .pipe(sass({
+            outputStyle: 'compressed',
+            sourceMap: true
+        }))
+        .on('error', function(err) {
+            gutil.log(gutil.colors.red(err));
+            gutil.beep();
+            this.emit('end');
+        })
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest(pkg.css));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('sass/*.scss', ['sass']);
+    //gulp.watch('js/**/*.js', ['js']);
+});
+
+gulp.task('default', ['watch']);
